84f86ae86c4b6b4170a2cbd11a758e2a42523324
good edu <goodeducoop@gmail.com>
2017-07-04 07:46:18 +0900

    Animation 연습

---
+++ app/src/main/java/com/nawoori/android/animation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.nawoori.android.animation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Animation flowAnim;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        flowAnim = AnimationUtils.loadAnimation(this, R.anim.flow);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.startAnimation(flowAnim);
+            }
+        });
+
+
+    }
+}
